db9b2465fc12492a90e70c20e7dc1d1dca9caf77
Christian Vermeulen <info@christianvermeulen.net>
2013-04-15 10:38:07 +0200

    added html5boilerplate for front and backend

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SilverkixCMSBundle:Default:index.html.twig');
+        return $this->render('SilverkixCMSBundle:Site:index.html.twig');
     }
 }
