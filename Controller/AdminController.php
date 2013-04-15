1e4f3b58fb42ecb264b604fe2f96d6f7d35fee77
Christian Vermeulen <info@christianvermeulen.net>
2013-04-15 13:06:20 +0200

    added admin login

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -3,12 +3,38 @@
 namespace Silverkix\CMSBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class AdminController extends Controller
 {
     public function indexAction()
     {
         return $this->render('SilverkixCMSBundle:Admin:index.html.twig');
+    }
+
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            'SilverkixCMSBundle:Admin:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+            )
+        );
     }
 
 }
