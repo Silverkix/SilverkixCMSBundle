cb3cac1b24f7a8775a0f75c285bd8015b1f89af8
Christian Vermeulen <info@christianvermeulen.net>
2013-04-25 13:38:04 +0200

    added simple pages to datafixtures for initial install

--- DataFixtures/ORM/LoadPageData.php
+++ DataFixtures/ORM/LoadPageData.php
@@ -14,28 +14,67 @@ class LoadPageData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         // Create a Parent page
-        $page = new Page();
+        $homePage = new Page();
 
-        $page->setTitle("Testpagina");
-        $page->setSlug('');
-        $page->setKeywords("Some, weird, keywords");
-        $page->setDescription("A really useful description of this awesome page");
-        $page->setContent("<h2>Subheading</h2><p>And a paragraphe</p>");
-        $page->setOrderid(1);
+        $homePage->setTitle("Home");
+        $homePage->setSlug('');
+        $homePage->setKeywords("cms, silverkix, new");
+        $homePage->setDescription("Welcome to your brand new CMS-enabled page.");
+        $homePage->setContent('<p>Hi there, thanks for using the <a href="https://github.com/Silverkix/SilverkixCMSBundle" target="_blank">SilverkixCMSBundle</a>! It seems you have set everything up correctly and are all ready to go from here. All that&#39;s left to do is overwrite the front-end template and create a new admin user.</p>');
+        $homePage->setOrderid(1);
+        $homePage->setOnline(true);
+
+        $manager->persist($homePage);
+
+        // Create a second page
+        $page = new Page();
+        $page->setTitle("About us");
+        $page->setSlug("about-us");
+        $page->setKeywords("about, us");
+        $page->setDescription("Some content telling more about the owner of the website.");
+        $page->setContent('<p>Hi there, thanks for using the <a href="https://github.com/Silverkix/SilverkixCMSBundle" target="_blank">SilverkixCMSBundle</a>! It seems you have set everything up correctly and are all ready to go from here. All that&#39;s left to do is overwrite the front-end template and create a new admin user.</p>');
         $page->setOnline(true);
+        $page->setOrderid(2);
 
         $manager->persist($page);
 
-        $page2 = new Page();
-
         // Create a child page
-        $page2->setTitle("Nog een pagina");
-        $page2->setSlug("nog-een-pagina");
-        $page2->setKeywords("more, useless keywords");
-        $page2->setDescription("This becomes a description");
-        $page2->setContent("<h2>Subheading</h2><p>And a paragraphe</p>");
+        $childPage = new Page();
+        $childPage->setTitle("Our history");
+        $childPage->setSlug("our-history");
+        $childPage->setKeywords("our, history");
+        $childPage->setDescription("Some content telling more about the owner of the website.");
+        $childPage->setContent('<p>Hi there, thanks for using the <a href="https://github.com/Silverkix/SilverkixCMSBundle" target="_blank">SilverkixCMSBundle</a>! It seems you have set everything up correctly and are all ready to go from here. All that&#39;s left to do is overwrite the front-end template and create a new admin user.</p>');
+        $childPage->setOnline(true);
+        $childPage->setOrderid(1);
+        $childPage->setParent($page);
+
+        $page->addChildren($childPage);
+        $manager->persist($childPage);
+
+        // Create another child page
+        $childPage2 = new Page();
+        $childPage2->setTitle("Our Future");
+        $childPage2->setSlug("our-future");
+        $childPage2->setKeywords("our, future");
+        $childPage2->setDescription("Some content telling more about the owner of the website.");
+        $childPage2->setContent('<p>Hi there, thanks for using the <a href="https://github.com/Silverkix/SilverkixCMSBundle" target="_blank">SilverkixCMSBundle</a>! It seems you have set everything up correctly and are all ready to go from here. All that&#39;s left to do is overwrite the front-end template and create a new admin user.</p>');
+        $childPage2->setOnline(true);
+        $childPage2->setOrderid(2);
+        $childPage2->setParent($page);
+
+        $page->addChildren($childPage2);
+        $manager->persist($childPage2);
+
+        // Create a second page
+        $page2 = new Page();
+        $page2->setTitle("Contact");
+        $page2->setSlug("contact");
+        $page2->setKeywords("contact, us");
+        $page2->setDescription("Some content telling more about the owner of the website.");
+        $page2->setContent('<p>Hi there, thanks for using the <a href="https://github.com/Silverkix/SilverkixCMSBundle" target="_blank">SilverkixCMSBundle</a>! It seems you have set everything up correctly and are all ready to go from here. All that&#39;s left to do is overwrite the front-end template and create a new admin user.</p>');
         $page2->setOnline(true);
-        $page2->setOrderid(2);
+        $page2->setOrderid(3);
 
         $manager->persist($page2);
 
